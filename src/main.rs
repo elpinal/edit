e37d193ac0022620798ec5a005d77e4fb2c2e059
elpinal <elpinal@users.noreply.github.com>
2017-08-03 21:04:49 +0900

    Add Editor struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+struct Editor {
+    buffer: String,
+    position: u32,
+}
+
+fn move_left(editor: Editor, n: u32) -> Editor {
+    Editor {
+        position: editor.position-n,
+        ..editor
+    }
+}
+
+fn move_right(editor: Editor, n: u32) -> Editor {
+    Editor {
+        position: editor.position+n,
+        ..editor
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let editor = Editor {
+        buffer: String::from("Hello, world!"),
+        position: 6,
+    };
+    let editor = move_right(editor, 1);
+    let editor = move_left(editor, 2);
+    println!("editor: {} {}", editor.buffer, editor.position);
 }
