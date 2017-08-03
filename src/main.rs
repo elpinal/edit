76aeab4eae4dbd51757c921208f32f8e30243fd1
elpinal <elpinal@users.noreply.github.com>
2017-08-03 22:24:54 +0900

    Support vertical motions

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,16 @@
 struct Editor {
     buffer: String,
+    newline_indices: Vec<u32>,
     line: u32,
     column: u32,
 }
 
 fn build_editor(buffer: String, line: u32, column: u32) -> Editor {
+    let mut indices: Vec<u32> = buffer.match_indices('\n').map(|(a, _)| a as u32).collect();
+    indices.push(buffer.len() as u32);
     Editor {
-        buffer,
+        buffer: buffer.clone(),
+        newline_indices: indices,
         line,
         column,
     }
@@ -27,7 +31,7 @@ fn move_left(editor: Editor, n: u32) -> Editor {
 
 fn move_right(editor: Editor, n: u32) -> Editor {
     // TODO: How about try_from?
-    let len: u32 = editor.buffer.len() as u32;
+    let len: u32 = editor.newline_indices[editor.line as usize];
     if editor.column + n > len {
         return Editor {
             column: len,
@@ -40,13 +44,42 @@ fn move_right(editor: Editor, n: u32) -> Editor {
     }
 }
 
+fn move_up(editor: Editor, n: u32) -> Editor {
+    if editor.line < n {
+        return Editor {
+            line: 0,
+            ..editor
+        };
+    }
+    Editor {
+        line: editor.line-n,
+        ..editor
+    }
+}
+
+fn move_down(editor: Editor, n: u32) -> Editor {
+    let len: u32 = editor.newline_indices.len() as u32;
+    if editor.line + n > len {
+        return Editor {
+            line: len - 1,
+            ..editor
+        };
+    }
+    Editor {
+        line: editor.line-n,
+        ..editor
+    }
+}
+
 fn main() {
     let editor = build_editor(
-        String::from("Hello, world!"),
-        0, 
+        String::from("Hello, world!\nThe 2nd line."),
+        1,
         6,
     );
     let editor = move_right(editor, 1);
     let editor = move_left(editor, 2);
+    let editor = move_up(editor, 1);
+    let editor = move_down(editor, 4);
     println!("editor: {} {} {}", editor.buffer, editor.line, editor.column);
 }
