38c2fe3882a13820731fe1f52b65d97f87e90073
elpinal <elpinal@users.noreply.github.com>
2017-08-05 15:09:52 +0900

    Change behavior of delete_at

--- src/editor.rs
+++ src/editor.rs
@@ -94,10 +94,27 @@ impl Editor {
     }
 
     pub fn delete_at(&mut self, line: u32, column: u32) {
-        let offset = self.offset(line, column).unwrap();
+        let line_width = self.line_width(line);
+        if line_width.is_none() {
+            return;
+        }
+        let line_width = line_width.unwrap();
+        if self.line_count() <= line || line_width < column {
+            return;
+        }
+        let current_offset = self.offset(self.line, self.column).expect(&format!("current_offset: {} {}", self.line, self.column));
+        if self.line == line && self.column == line_width {
+            self.column -= 1;
+        }
+        let width = self.line_width(line).expect(&format!("width: {}", line));
+        let offset = self.offset(line, column).expect(&format!("offset: {} {}", line, column));
         let ch = self.buffer.remove(offset as usize);
         if ch == '\n' {
             self.newline_indices.remove(line as usize);
+            if offset <= current_offset {
+                self.line -= 1;
+                self.column = width;;
+            }
         }
         for x in self.newline_indices.iter_mut() {
             if *x > offset {
@@ -307,6 +324,37 @@ mod tests {
                 String::from("Hello,world!The 2nd line.\nAAABBBCCC."),
                 0,
                 6,
+                )
+            );
+
+        let mut editor = build_editor(String::from("abc\ndef"), 0, 3);
+        editor.delete_at(0, 2);
+        assert_eq!(
+            editor,
+            build_editor(
+                String::from("ab\ndef"),
+                0,
+                2,
+                )
+            );
+
+        let mut editor = build_editor(String::from("abc\ndef"), 1, 0);
+        editor.delete_at(0, 3);
+        assert_eq!(
+            editor,
+            build_editor(
+                String::from("abcdef"),
+                0,
+                3,
+                )
+            );
+        editor.delete_at(10, 10);
+        assert_eq!(
+            editor,
+            build_editor(
+                String::from("abcdef"),
+                0,
+                3,
                 )
             );
     }
