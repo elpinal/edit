d44f292e674408a9f1b2d26eeb9bf44b37b76d75
elpinal <elpinal@users.noreply.github.com>
2017-08-09 11:34:09 +0900

    Add more example

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,15 @@ use editor::*;
 
 fn main() {
     let mut editor = new(String::from("Hello, world!\nThe 2nd line."), 1, 6).unwrap();
+    editor.move_to_end();
     editor.move_right(1);
     editor.move_left(2);
+    editor.move_to_beginning();
     editor.move_up(1);
     editor.move_down(4);
     editor.insert_at('4', 1, 4);
     editor.insert_at('4', 0, 0);
+    editor.delete_at(0, 13);
     println!(
         "editor: {} {} {}",
         editor.buffer(),
