868ceff09bc966c83cbd27a768ccdde8777ea919
elpinal <elpinal@users.noreply.github.com>
2017-08-09 11:43:23 +0900

    Add more example

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,8 @@ fn main() {
     editor.insert_at('4', 1, 4);
     editor.insert_at('4', 0, 0);
     editor.delete_at(0, 13);
+    let width = editor.line_width(1).unwrap();
+    editor.insert_string_at("\nThe 3rd line.", 1, width);
     println!(
         "editor: {} {} {}",
         editor.buffer(),
