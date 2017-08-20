02596beb13dc5b53e22e8425fe25e70526ec393e
elpinal <elpinal@users.noreply.github.com>
2017-08-20 22:35:12 +0900

    Avoid panic of sort_line

--- src/editor/mod.rs
+++ src/editor/mod.rs
@@ -932,23 +932,24 @@ impl Editor {
     /// assert_eq!(editor.column(), 3);
     /// ```
     pub fn sort_line(&mut self) {
+        let l = self.line();
+        let nl: usize;
         let mut buf = String::new();
         {
             let mut vec = Vec::new();
             for l in 0..self.core.line_count() {
-                vec.push(self.line_buffer(l).unwrap());
+                vec.push((l, self.line_buffer(l).unwrap()));
             }
-            vec.sort();
-            for s in vec.into_iter() {
+            vec.sort_by_key(|&(_, v)| v);
+            nl = vec.iter().position(|&(i, _)| i == l).unwrap();
+            for (_, s) in vec.into_iter() {
                 let s: String = s.iter().collect();
                 buf += &s;
                 buf += "\n";
             }
         }
-        // FIXME: Possibility of panic: `l` should be updated.
-        let l = self.line();
         let c = self.column();
-        self.core.reset(&buf, l, c);
+        self.core.reset(&buf, nl, c);
     }
 
     /// Sort lines in a range.
@@ -1199,5 +1200,29 @@ mod tests {
         buf.push('\n');
         let editor = Editor::new(&buf, 1, 6).unwrap();
         test(editor);
+    }
+
+    #[test]
+    fn test_sort_line() {
+        let mut editor = Editor::new(
+            "a a\n\
+             c c\n\
+             b b b\n\
+             c a",
+            2,
+            4,
+        ).unwrap();
+        editor.sort_line();
+
+        let buf: String = editor.buffer().iter().collect();
+        assert_eq!(
+            buf,
+            "a a\n\
+             b b b\n\
+             c a\n\
+             c c\n"
+        );
+        assert_eq!(editor.line(), 1);
+        assert_eq!(editor.column(), 4);
     }
 }
