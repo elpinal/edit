31d066baf23d16928ba2f7c6627a2cd73ab3b850
elpinal <elpinal@users.noreply.github.com>
2017-08-22 23:34:53 +0900

    Add Core2

---
+++ src/editor/core2.rs
@@ -0,0 +1,41 @@
+#[derive(PartialEq, Debug)]
+pub struct Core2 {
+    buffer: Vec<Vec<char>>,
+    line: usize,
+    column: usize,
+}
+
+impl Core2 {
+    pub fn new(buffer: &str, line: usize, column: usize) -> Result<Core2, String> {
+        let buf: Vec<Vec<char>> = buffer.lines().map(|l| l.chars().collect()).collect();
+
+        if buf.len() <= line {
+            return Err(format!(
+                "Line {} is out of range [0, {})",
+                line,
+                buf.len()
+            ));
+        }
+        let width = buf[line].len();
+        if width < column {
+            return Err(format!("Column {} is out of range [0, {}]", column, width));
+        }
+        Ok(Core2 {
+            buffer: buf,
+            line,
+            column,
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_new() {
+        let buffer = "aa aa";
+        let editor = Core2::new(buffer, 0, 0);
+        assert!(editor.is_ok());
+    }
+}
